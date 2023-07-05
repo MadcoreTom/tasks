14fd7d182077f7412c43f8ca6d21ec9352a4ac68
MadcoreTom <none>
2023-07-05 19:14:11 +1000

    Updated the save dialog itself to Primer

--- src/components/save.tsx
+++ src/components/save.tsx
@@ -3,19 +3,32 @@ import { useDispatch, useSelector } from "react-redux";
 import { importGraph, RootState, saveLocalGraph, setTitle, updateSaveDialog } from "../state/store";
 import { LOCAL_STORAGE } from "../util/local-storage";
 import { ImportButton } from "./import.button";
-import { Box, Button, ButtonGroup, FormControl, Octicon, TextInput, TreeView } from "@primer/react";
+import { Box, Button, ButtonGroup, Dialog, FormControl, TextInput, TreeView } from "@primer/react";
+import { FileIcon } from "@primer/octicons-react";
 
 export function BrowserSaveContainer() {
     const saveDialogState = useSelector((state: RootState) => state.main.saveDialog);
     const title = useSelector((state: RootState) => state.main.title);
+    const dispatch = useDispatch();
+
 
     if (!saveDialogState.show) {
         return null;
     } else {
-        return <BrowserSave initialName={title} files={saveDialogState.files} showNew={saveDialogState.showNew} />
+        return <Dialog
+            isOpen={true}
+            sx={{minWidth:saveDialogState.showNew ? 700 : 300}}
+            onDismiss={() => dispatch(updateSaveDialog({ show: false, updateFiles: false }))}
+            aria-labelledby="header-id"  >
+            <Dialog.Header id="header-id">{saveDialogState.showNew ? "New" : "Files"}</Dialog.Header>
+            <Box>
+                <BrowserSave initialName={title} files={saveDialogState.files} showNew={saveDialogState.showNew} />
+            </Box>
+        </Dialog> 
     }
 
 }
+
 
 function BrowserSave(props: { initialName: string, files: string[], showNew: boolean }) {
     const [selected, setSelected] = React.useState(props.initialName);
@@ -23,10 +36,6 @@ function BrowserSave(props: { initialName: string, files: string[], showNew: boo
 
     const files = props.files;
     const selectedInList = !!selected && files.indexOf(selected) >= 0;
-
-    function close() {
-        dispatch(updateSaveDialog({ show: false, updateFiles: false }));
-    }
 
     function saveAs() {
         if (!selectedInList && selected != undefined && selected.length > 0) {
@@ -63,19 +72,16 @@ function BrowserSave(props: { initialName: string, files: string[], showNew: boo
         }
     }
 
-    let title:string;
     let newSection:JSX.Element | null = null;
     let saveLoadOptions:JSX.Element;
     let setName:JSX.Element | null = null;
 
     if (props.showNew) {
-        title = "New";
         newSection = <NewDialog />
         saveLoadOptions = <ButtonGroup sx={{ p: 2 }}>
             <Button onClick={load} disabled={!selectedInList}>Load</Button>
         </ButtonGroup>
     } else {
-        title = "Browser Storage";
         saveLoadOptions = <ButtonGroup sx={{ p: 2 }}>
             <Button onClick={load} disabled={!selectedInList} >Load</Button>
             <Button onClick={saveOver} disabled={!selectedInList}>Save</Button>
@@ -89,26 +95,17 @@ function BrowserSave(props: { initialName: string, files: string[], showNew: boo
     }
 
 
-    return <div className="modal is-active">
-        <div className="modal-background" onClick={close}></div>
-        <div className="modal-card">
-            <header className="modal-card-head">
-                <p className="modal-card-title">{title}</p>
-                <button className="delete" aria-label="close" onClick={close} />
-            </header>
-            <div style={{ display: "flex" }}>
-                {newSection}
-                <section className="modal-card-body browser-storage" style={{ minWidth: 200, minHeight: 200 }}>
-                    <FileList selected={selected} files={files} setSelected={setSelected} />
-                    {setName}
-                    {saveLoadOptions}
-                </section>
-            </div>
-        </div>
+    return <div style={{ display: "flex" }}>
+        {newSection}
+        <section className="modal-card-body browser-storage">
+            <FileList selected={selected} files={files} setSelected={setSelected} />
+            {setName}
+            {saveLoadOptions}
+        </section>
     </div>
 }
 
-function NewDialog(props: {}): JSX.Element {
+function NewDialog(): JSX.Element {
     const dispatch = useDispatch();
 
     return <section className="modal-card-body new-section">
@@ -129,7 +126,7 @@ function FileList(props: { selected: string, files: string[], setSelected: (sele
             onSelect={() => setSelected(f)}
             current={f == selected}>
             <TreeView.LeadingVisual>
-                {/* <FileIcon /> */}icon
+                <FileIcon />
             </TreeView.LeadingVisual>
             {f}
         </TreeView.Item>
